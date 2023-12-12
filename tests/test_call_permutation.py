c432efb4047f30636fe6cdc7b9ab43d407548a16
raphsenn <raphaelsenn@outlook.de>
2023-12-12 20:48:33 +0100

    test

--- tests/test_call_permutation.py
+++ tests/test_call_permutation.py
@@ -22,3 +22,17 @@ def test_call_permutation_3():
     assert p(3) == 1
     assert p(4) == 2
     assert p(5) == 4
+
+
+def test_call_permutation_4():
+    p = Permutation(1, 2, 3, 4, 5, 6, 7, 8, 9, 10)
+    assert p(1) == 1
+    assert p(2) == 2
+    assert p(3) == 3
+    assert p(4) == 4
+    assert p(5) == 5
+    assert p(6) == 6
+    assert p(7) == 7
+    assert p(8) == 8
+    assert p(9) == 9
+    assert p(10) == 10
